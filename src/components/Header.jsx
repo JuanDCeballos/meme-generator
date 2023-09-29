0a6a8ff3bb4a4812fdb8e151b88062c5e2459a92
Juan Ceballos <juandavi789@hotmail.com>
2023-09-29 17:54:33 -0500

    Update wrong text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className='header'>
       <div className='header-title'>
         <img className='header-title-img' src='./troll-face.png' />
-        <h1 className='title'>MemeGenerator</h1>
+        <h1 className='title'>Meme Generator</h1>
       </div>
       <div className='header-desc'>
         <p className='desc'>React Course - Project 3</p>
